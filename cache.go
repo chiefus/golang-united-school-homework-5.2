cd11d6fdcbf7f0c43b98a88b65d3d8e601c857e9
Andrei Shalkou <andrei.shalkou@camelotls.com>
2022-05-20 17:18:34 +0300

    [Draft] Initial implementation without sync

--- cache.go
+++ cache.go
@@ -1,23 +1,57 @@
 package cache
 
-import "time"
+import (
+	"time"
+)
+
+type Item struct {
+	Value    string
+	deadline *time.Time
+}
 
 type Cache struct {
+	items map[string]Item
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{
+		items: map[string]Item{},
+	}
 }
 
-func (receiver) Get(key string) (string, bool) {
+func (cache *Cache) Get(key string) (string, bool) {
+	_, ok := cache.items[key]
+	if !ok {
+		return "", false
+	}
 
+	now := time.Now()
+	if cache.items[key].deadline != nil && cache.items[key].deadline.Before(now) {
+		return "", false
+	}
+
+	return cache.items[key].Value, true
 }
 
-func (receiver) Put(key, value string) {
+func (cache *Cache) Put(key, value string) {
+	cache.items[key] = Item{Value: value, deadline: nil}
 }
 
-func (receiver) Keys() []string {
+func (cache *Cache) Keys() []string {
+	var keys []string
+	now := time.Now()
+
+	for k, v := range cache.items {
+		if v.deadline != nil && v.deadline.Before(now) {
+			continue
+		}
+
+		keys = append(keys, k)
+	}
+
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (cache *Cache) PutTill(key, value string, deadline time.Time) {
+	cache.items[key] = Item{value, &deadline}
 }
